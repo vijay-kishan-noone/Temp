98e0c7b4bcf6f4bbdae1bfebc8252b6a0e3df891
Vijay Kishan <vijaykishan333@gmail.com>
2021-10-12 18:25:03 +0530

    Changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Phase4Assessment.Models;
 using System;
@@ -13,6 +12,8 @@ namespace Phase4Assessment.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static readonly PizzaBO context = new PizzaBO();
+        private static List<PizzaModel> pizzas = context.GetAllPizzas();
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -20,12 +21,15 @@ namespace Phase4Assessment.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(pizzas);
         }
-
-        public IActionResult Privacy()
+        public IActionResult Details(int id)
         {
-            return View();
+            return View(context.GetPizzaById(id));
+        }
+        public IActionResult Order(int id)
+        {
+            return View(context.GetPizzaById(id));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/PizzaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phase4Assessment.Models
+{
+    public class PizzaModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
