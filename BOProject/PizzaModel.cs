627ff173caf03d3767edcab68733a23b95286941
Vijay Kishan <vijaykishan333@gmail.com>
2021-10-12 17:42:12 +0530

    "Changes"

---
+++ BOProject/PizzaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOProject
+{
+    class PizzaModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
